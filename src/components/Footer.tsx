e3b577b26a0681203a62da7c6a4fa2b5b66b06cb
Bruno Luis <bruno-aqv@outlook.com>
2021-03-10 00:28:53 -0300

    template completo

---
+++ src/components/Footer.tsx
@@ -0,0 +1,96 @@
+import { StyledFooter } from "../styles/Footer";
+export function Footer() {
+  return (
+    <div>
+      <StyledFooter>
+        <footer>
+          <div className="bg">
+            <div className="containertext">
+              <h1>growth plans</h1>
+              <br />
+              <p>take your business to the next level</p>
+            </div>
+            <div className="container">
+              <div className=" wh-600 ">
+                <div className="cima">
+                  <h1>$ 100 </h1>
+                  <h2>100 + hours</h2>
+                </div>
+                <div className="baixo">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+
+              <div className=" wh-500 ">
+                <div className="cima">
+                  {" "}
+                  <h1>$ 100 </h1>
+                  <h2>100 + hours</h2>
+                </div>
+                <div className="baixo">
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+
+              <div className=" wh-400 ">
+                <div className="cima">
+                  {" "}
+                  <h1>$ 100 </h1>
+                  <h2>100 + hours</h2>
+                </div>
+                <div className="baixo">
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+              <div></div>
+              <div className=" wh-300 ">
+                <div className="cima">
+                  <h1>$ 100 </h1>
+                  <h2>100 + hours</h2>
+                </div>
+                <div className="baixo">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+
+              <div className=" wh-200 ">
+                <div className="cima">
+                  {" "}
+                  <h1>$ 100 </h1>
+                  <h2>100 + hours</h2>
+                </div>
+                <div className="baixo">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+
+              <div className=" wh-100 ">
+                <div className="cima">
+                  <h1>$ 100 </h1>
+                  <h2>100 + hours</h2>
+                </div>
+                <div className="baixo">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </StyledFooter>
+    </div>
+  );
+}
---
+++ src/styles/Footer.tsx
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  footer {
+      margin: 5% 0% 5% 0%; 
+
+  }
+
+  .bg {
+    background-color: #e9e9e9;
+  }
+
+  .container {
+    /* margin: 5% 2% 5% 5%; */
+    margin: 0% 5% 0% 5%;
+
+    -webkit-align-items: flex-end;
+    align-items: flex-end;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  .containertext {
+    /* margin: 5% 2% 0% 5%; */
+    align-items: flex-end;
+    margin: 0% 5% 0% 5%;
+  }
+  /* margin: 5% 2% 5% 5%; */
+
+  .baixo {
+    width: 100%;
+    background-color: #3399ff;
+    display: flex;
+    height: 100%;
+
+    align-items: flex-end;
+p{
+  text-align: center;
+    color: white;
+    font-family: 'Poppins', sans-serif;
+    margin: 5% 5% 5% 5%;
+}
+  }
+  .cima {
+    max-width: 100%;
+    width: 100%;
+    height: 100px;
+    background-color: #0b2fa2;
+    vertical-align: top;
+
+    h1,
+    h2 {
+      color: white;
+      line-height: 0.1;
+      text-align: justify;
+      padding: 10% 0 0 5%;
+      font-family: 'Poppins', sans-serif;
+    }
+
+    h2 {
+      font-size: 1rem;
+    }
+  }
+
+  .wh-600 {
+    width: 150px;
+    height: 450px;
+    display: flex;
+    flex-direction: column;
+  }
+  .wh-500 {
+    width: 150px;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+  }
+  .wh-400 {
+    width: 150px;
+    height: 350px;
+    display: flex;
+    flex-direction: column;
+  }
+  .wh-300 {
+    width: 150px;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+  }
+  .wh-200 {
+    width: 150px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .wh-100 {
+    width: 150px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+  }
+`;
